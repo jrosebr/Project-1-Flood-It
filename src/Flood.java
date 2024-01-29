515547abd2beeab6c163419e0128f395c4cfe690
jrosebr <jrosebr@iu.edu>
2024-01-29 10:35:37 -0500

    Updated Project

--- src/Flood.java
+++ src/Flood.java
@@ -10,12 +10,22 @@ public class Flood {
     public static void flood(WaterColor color,
                               LinkedList<Coord> flooded_list,
                               Tile[][] tiles,
-                              Integer board_size) {
-        int s = 0;
+                              Integer board_size)
+    {
         for (int i = 0; i < flooded_list.size(); ++i)
         {
-            System.out.println(s);
-            ++s;
+            List<Coord> neighbors = flooded_list.get(i).neighbors(board_size);
+
+            for (int j = 0; j < neighbors.size(); ++j)
+            {
+                if (!flooded_list.contains(neighbors.get(j)))
+                {
+                    if (Board.get(neighbors.get(j)).getColor().equals(color))
+                    {
+                        flooded_list.add(neighbors.get(j));
+                    }
+                }
+            }
         }
     }
 
